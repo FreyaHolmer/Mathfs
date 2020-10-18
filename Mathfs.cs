72d926c4e3fd33d3f3f5319a80114dca7e3cd6ba
Freya Holmér <acegikmo@gmail.com>
2020-10-18 16:10:30 +0200

    Triangle is now a separate file

--- Mathfs.cs
+++ Mathfs.cs
@@ -333,44 +333,6 @@ public static partial class Mathfs {
 	}
 
 
-	// Triangle math
-	public static class Triangle {
-
-		public static float SignedArea( Vector3 a, Vector3 b, Vector3 c ) => Vector3.Cross( b - a, c - a ).magnitude / 2f;
-		public static float Area( Vector3 a, Vector3 b, Vector3 c ) => Mathf.Abs( SignedArea( a, b, c ) );
-		public static Vector3 Centroid( Vector3 a, Vector3 b, Vector3 c ) => ( a + b + c ) / 3f;
-
-		public static Vector3 Incenter( Vector3 a, Vector3 b, Vector3 c ) {
-			float bc = Vector3.Distance( b, c );
-			float ca = Vector3.Distance( c, a );
-			float ab = Vector3.Distance( a, b );
-			return ( bc * a + ca * b + ab * c ) / ( bc + ca + ab );
-		}
-
-		public static (Vector2 center, float radius) Circumcircle( Vector2 a, Vector2 b, Vector2 c ) {
-			Vector2 SqSub( Vector2 p0, Vector2 p1 ) => Vector2.Scale( p0, p0 ) - Vector2.Scale( p1, p1 );
-			Vector2 BA = a - b;
-			Vector2 CA = a - c;
-			Vector2 s13 = SqSub( a, c ); // the heck is this
-			Vector2 s21 = SqSub( b, a );
-			Vector2 noot = BA * ( s13.x + s13.y ) + CA * ( s21.x + s21.y );
-			Vector2 center = new Vector2( noot.y, -noot.x ) / ( 2 * Determinant( CA, BA ) );
-			float boop = -a.x * a.x - a.y * a.y + 2 * center.x * a.x + 2 * center.y * a.y;
-			float radius = Mathf.Sqrt( center.sqrMagnitude - boop );
-			return ( center, radius );
-		}
-
-		public static bool Contains( Vector2 a, Vector2 b, Vector2 c, Vector2 point, float aMargin = 0f, float bMargin = 0f, float cMargin = 0f ) {
-			float d0 = Determinant( b - a, point - a );
-			float d1 = Determinant( c - b, point - b );
-			float d2 = Determinant( a - c, point - c );
-			bool b0 = d0 < cMargin;
-			bool b1 = d1 < aMargin;
-			bool b2 = d2 < bMargin;
-			return b0 == b1 && b1 == b2; // on the same side of all halfspaces, this can only happen inside
-		}
-
-	}
 
 	public static class Polygon {
 
---
+++ Triangle.cs
@@ -0,0 +1,54 @@
+﻿// Collected and expanded upon to by Freya Holmér (https://github.com/FreyaHolmer/Mathfs)
+
+using UnityEngine;
+
+public static partial class Mathfs {
+
+	// Triangle math
+	public static class Triangle {
+
+		public static float SignedArea( Vector3 a, Vector3 b, Vector3 c ) => Vector3.Cross( b - a, c - a ).magnitude / 2f;
+		public static float Area( Vector3 a, Vector3 b, Vector3 c ) => Mathf.Abs( SignedArea( a, b, c ) );
+		public static Vector3 Centroid( Vector3 a, Vector3 b, Vector3 c ) => ( a + b + c ) / 3f;
+
+		public static Vector3 Incenter( Vector3 a, Vector3 b, Vector3 c ) {
+			float bc = Vector3.Distance( b, c );
+			float ca = Vector3.Distance( c, a );
+			float ab = Vector3.Distance( a, b );
+			return ( bc * a + ca * b + ab * c ) / ( bc + ca + ab );
+		}
+
+		public static Circle Incircle( Vector2 a, Vector2 b, Vector2 c ) {
+			float bc = Vector2.Distance( b, c );
+			float ca = Vector2.Distance( c, a );
+			float ab = Vector2.Distance( a, b );
+			float sideSum = bc + ca + ab;
+			Vector2 incenter = ( bc * a + ca * b + ab * c ) / sideSum;
+			float s = sideSum * 0.5f;
+			float r = Sqrt( ( s - bc ) * ( s - ca ) * ( s - ab ) / s );
+			return new Circle( incenter, r );
+		}
+
+		public static bool Circumcircle( Vector2 a, Vector2 b, Vector2 c, out Circle circle ) => Circle.FromThreePoints( a, b, c, out circle );
+
+		public static bool Contains( Vector2 a, Vector2 b, Vector2 c, Vector2 point, float aMargin = 0f, float bMargin = 0f, float cMargin = 0f ) {
+			float d0 = Determinant( b - a, point - a );
+			float d1 = Determinant( c - b, point - b );
+			float d2 = Determinant( a - c, point - c );
+			bool b0 = d0 < cMargin;
+			bool b1 = d1 < aMargin;
+			bool b2 = d2 < bMargin;
+			return b0 == b1 && b1 == b2; // on the same side of all halfspaces, this can only happen inside
+		}
+
+		public static float SmallestAngle( Vector3 a, Vector3 b, Vector3 c ) {
+			float angA = AngleBetween( c - b, a - b );
+			float angB = AngleBetween( b - c, a - c );
+			float angC = PI - angA - angB;
+			return Min( angA, angB, angC );
+		}
+
+	}
+
+
+}
