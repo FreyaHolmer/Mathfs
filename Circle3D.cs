fdf3dc58745fdc7a92ca079165e2a34dbffb2d4a
Freya Holmér <acegikmo@gmail.com>
2021-06-21 13:30:34 +0200

    added 3D oriented circle, Circle3D

---
+++ Circle3D.cs
@@ -0,0 +1,25 @@
+﻿// Collected and expanded upon to by Freya Holmér (https://github.com/FreyaHolmer/Mathfs)
+
+using UnityEngine;
+using static Freya.Mathfs;
+
+namespace Freya {
+
+	// Circle math
+	public struct Circle3D {
+		public Vector3 center;
+		public Vector3 normal;
+		public float radius;
+
+		public Circle3D( Vector3 center, Vector3 normal, float radius ) {
+			this.center = center;
+			this.normal = normal;
+			this.radius = radius;
+		}
+
+		float Area => Circle.RadiusToArea( radius );
+		float Circumference => Circle.RadiusToCircumference( radius );
+
+	}
+
+}
