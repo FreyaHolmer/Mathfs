60f4011b1cfae65d29b10f95d41532d100dd4ee9
Freya Holmér <acegikmo@gmail.com>
2021-06-23 16:17:28 +0200

    reorganized MathfsExtensions a lil

--- Extensions.cs
+++ Extensions.cs
@@ -6,10 +6,10 @@ namespace Freya {
 
 	public static class MathfsExtensions {
 
+		// vector rotation and angles
 		public static float Angle( this Vector2 v ) => Mathf.Atan2( v.y, v.x );
 		public static Vector2 Rotate90CW( this Vector2 v ) => new Vector2( v.y, -v.x );
 		public static Vector2 Rotate90CCW( this Vector2 v ) => new Vector2( -v.y, v.x );
-
 		public static Vector2 RotateAround( this Vector2 v, Vector2 pivot, float angRad ) => Rotate( v - pivot, angRad ) + pivot;
 
 		public static Vector2 Rotate( this Vector2 v, float angRad ) {
@@ -26,6 +26,7 @@ namespace Freya {
 		public static Vector3 FlattenY( this Vector3 v ) => new Vector3( v.x, 0f, v.z );
 		public static Vector3 FlattenZ( this Vector3 v ) => new Vector3( v.x, v.y, 0f );
 
+		// per-component math operations
 		public static float Frac( this float v ) => v - Mathfs.Floor( v );
 		public static Vector2 Frac( this Vector2 v ) => v - v.Floor();
 		public static Vector3 Frac( this Vector3 v ) => v - v.Floor();
@@ -44,8 +45,32 @@ namespace Freya {
 		public static float Max( this Vector3 v ) => Mathfs.Max( v.x, v.y, v.z );
 		public static float Max( this Vector4 v ) => Mathfs.Max( v.x, v.y, v.z, v.w );
 
+		public static int Abs( this int v ) => Mathfs.Abs( v );
+		public static float Abs( this float v ) => Mathfs.Abs( v );
+		public static Vector2 Floor( this Vector2 v ) => new Vector2( Mathfs.Floor( v.x ), Mathfs.Floor( v.y ) );
+		public static Vector3 Floor( this Vector3 v ) => new Vector3( Mathfs.Floor( v.x ), Mathfs.Floor( v.y ), Mathfs.Floor( v.z ) );
+		public static Vector4 Floor( this Vector4 v ) => new Vector4( Mathfs.Floor( v.x ), Mathfs.Floor( v.y ), Mathfs.Floor( v.z ), Mathfs.Floor( v.w ) );
+		public static int FloorToInt( this float v ) => Mathfs.FloorToInt( v );
+		public static Vector2 Ceil( this Vector2 v ) => new Vector2( Mathfs.Ceil( v.x ), Mathfs.Ceil( v.y ) );
+		public static Vector3 Ceil( this Vector3 v ) => new Vector3( Mathfs.Ceil( v.x ), Mathfs.Ceil( v.y ), Mathfs.Ceil( v.z ) );
+		public static Vector4 Ceil( this Vector4 v ) => new Vector4( Mathfs.Ceil( v.x ), Mathfs.Ceil( v.y ), Mathfs.Ceil( v.z ), Mathfs.Ceil( v.w ) );
+		public static int CeilToInt( this float v ) => Mathfs.CeilToInt( v );
+		public static float Round( this float v, float snapInterval ) => Mathfs.Round( v, snapInterval );
+		public static Vector2 Round( this Vector2 v ) => new Vector2( Mathfs.Round( v.x ), Mathfs.Round( v.y ) );
+		public static Vector3 Round( this Vector3 v ) => new Vector3( Mathfs.Round( v.x ), Mathfs.Round( v.y ), Mathfs.Round( v.z ) );
+		public static Vector4 Round( this Vector4 v ) => new Vector4( Mathfs.Round( v.x ), Mathfs.Round( v.y ), Mathfs.Round( v.z ), Mathfs.Round( v.w ) );
+		public static int RoundToInt( this float v ) => Mathfs.RoundToInt( v );
+		public static Vector2Int RoundToInt( this Vector2 v ) => new Vector2Int( Mathf.RoundToInt( v.x ), Mathf.RoundToInt( v.y ) );
+		public static Vector3Int RoundToInt( this Vector3 v ) => new Vector3Int( Mathf.RoundToInt( v.x ), Mathf.RoundToInt( v.y ), Mathf.RoundToInt( v.z ) );
+		public static float Remap( this float v, float iMin, float iMax, float oMin, float oMax ) => Mathfs.Remap( iMin, iMax, oMin, oMax, v );
+		// vector direction & magnitude functions
+		public static float Magnitude( this float v ) => Mathfs.Abs( v );
 		public static Vector2 WithMagnitude( this Vector2 v, float mag ) => v.normalized * mag;
 		public static Vector3 WithMagnitude( this Vector3 v, float mag ) => v.normalized * mag;
+		public static Vector2 To( this Vector2 v, Vector2 target ) => target - v;
+		public static Vector3 To( this Vector3 v, Vector3 target ) => target - v;
+		public static Vector2 DirTo( this Vector2 v, Vector2 target ) => ( target - v ).normalized;
+		public static Vector3 DirTo( this Vector3 v, Vector3 target ) => ( target - v ).normalized;
 
 		public static (Vector2 dir, float magnitude ) GetDirAndMagnitude( this Vector2 v ) {
 			float magnitude = v.magnitude;
@@ -59,58 +84,19 @@ namespace Freya {
 
 		public static Vector2 ClampMagnitude( this Vector2 v, float min, float max ) {
 			float mag = v.magnitude;
-			if( mag < min ) {
-				Vector2 dir = v / mag;
-				return dir * min;
-			}
-
-			if( mag > max ) {
-				Vector2 dir = v / mag;
-				return dir * max;
-			}
-
-			return v;
+			return mag < min ? ( v / mag ) * min : mag > max ? ( v / mag ) * max : v;
 		}
 
 		public static Vector3 ClampMagnitude( this Vector3 v, float min, float max ) {
 			float mag = v.magnitude;
-			if( mag < min ) {
-				Vector3 dir = v / mag;
-				return dir * min;
-			}
-
-			if( mag > max ) {
-				Vector3 dir = v / mag;
-				return dir * max;
-			}
-
-			return v;
+			return mag < min ? ( v / mag ) * min : mag > max ? ( v / mag ) * max : v;
 		}
-
-
-		public static Vector2 To( this Vector2 v, Vector2 target ) => target - v;
-		public static Vector3 To( this Vector3 v, Vector3 target ) => target - v;
-		public static Vector2 DirTo( this Vector2 v, Vector2 target ) => ( target - v ).normalized;
-		public static Vector3 DirTo( this Vector3 v, Vector3 target ) => ( target - v ).normalized;
-
-		public static Vector2 Floor( this Vector2 v ) => new Vector2( Mathfs.Floor( v.x ), Mathfs.Floor( v.y ) );
-		public static Vector3 Floor( this Vector3 v ) => new Vector3( Mathfs.Floor( v.x ), Mathfs.Floor( v.y ), Mathfs.Floor( v.z ) );
-		public static Vector4 Floor( this Vector4 v ) => new Vector4( Mathfs.Floor( v.x ), Mathfs.Floor( v.y ), Mathfs.Floor( v.z ), Mathfs.Floor( v.w ) );
-		public static Vector2 Ceil( this Vector2 v ) => new Vector2( Mathfs.Ceil( v.x ), Mathfs.Ceil( v.y ) );
-		public static Vector3 Ceil( this Vector3 v ) => new Vector3( Mathfs.Ceil( v.x ), Mathfs.Ceil( v.y ), Mathfs.Ceil( v.z ) );
-		public static Vector4 Ceil( this Vector4 v ) => new Vector4( Mathfs.Ceil( v.x ), Mathfs.Ceil( v.y ), Mathfs.Ceil( v.z ), Mathfs.Ceil( v.w ) );
-		public static Vector2 Round( this Vector2 v ) => new Vector2( Mathfs.Round( v.x ), Mathfs.Round( v.y ) );
-		public static Vector2Int RoundToInt( this Vector2 v ) => new Vector2Int( Mathf.RoundToInt( v.x ), Mathf.RoundToInt( v.y ) );
-		public static Vector3 Round( this Vector3 v ) => new Vector3( Mathfs.Round( v.x ), Mathfs.Round( v.y ), Mathfs.Round( v.z ) );
-		public static Vector3Int RoundToInt( this Vector3 v ) => new Vector3Int( Mathf.RoundToInt( v.x ), Mathf.RoundToInt( v.y ), Mathf.RoundToInt( v.z ) );
-		public static Vector4 Round( this Vector4 v ) => new Vector4( Mathfs.Round( v.x ), Mathfs.Round( v.y ), Mathfs.Round( v.z ), Mathfs.Round( v.w ) );
 
 		public static Color WithAlpha( this Color c, float a ) => new Color( c.r, c.g, c.b, a );
 		public static Color MultiplyRGB( this Color c, float m ) => new Color( c.r * m, c.g * m, c.b * m, c.a );
 		public static Color MultiplyRGB( this Color c, Color m ) => new Color( c.r * m.r, c.g * m.g, c.b * m.b, c.a );
 		public static Color MultiplyA( this Color c, float m ) => new Color( c.r, c.g, c.b, c.a * m );
 
-		public static float Round( this float v, float snapInterval ) => Mathfs.Round( v, snapInterval );
 		public static bool Within( this float v, float min, float max ) => v >= min && v <= max;
 		public static bool Between( this float v, float min, float max ) => v > min && v < max;
 		public static bool Within( this int v, int min, int max ) => v >= min && v <= max;
@@ -122,19 +108,12 @@ namespace Freya {
 
 		public static float Square( this float v ) => v * v;
 		public static int Square( this int v ) => v * v;
-		public static float Abs( this float v ) => Mathfs.Abs( v );
-		public static int Abs( this int v ) => Mathfs.Abs( v );
-		public static float Magnitude( this float v ) => Mathfs.Abs( v );
 		public static float Clamp( this float v, float min, float max ) => Mathfs.Clamp( v, min, max );
 		public static int Clamp( this int v, int min, int max ) => Mathfs.Clamp( v, min, max );
-		public static float Remap( this float v, float iMin, float iMax, float oMin, float oMax ) => Mathfs.Remap( iMin, iMax, oMin, oMax, v );
 		public static float Repeat( this float v, float length ) => Mathfs.Repeat( v, length );
 		public static int Mod( this int value, int length ) => ( value % length + length ) % length; // modulo
 		public static int NextMod( this int value, int length ) => Mod( value + 1, length );
 		public static int PrevMod( this int value, int length ) => Mod( value - 1, length );
-		public static int RoundToInt( this float v ) => Mathfs.RoundToInt( v );
-		public static int FloorToInt( this float v ) => Mathfs.FloorToInt( v );
-		public static int CeilToInt( this float v ) => Mathfs.CeilToInt( v );
 
 
 	}
