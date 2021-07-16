3cab638ab61e9de57a4e8b8efbb2865d8739b8fa
Freya Holmér <acegikmo@gmail.com>
2021-07-16 10:58:09 +0200

    Box size, min/max, volume, surface area

--- Geometric Shapes/Box.cs
+++ Geometric Shapes/Box.cs
@@ -39,4 +39,107 @@ namespace Freya {
 
 	}
 
+	#region Size
+
+	public partial struct Box2D {
+		/// <summary>The size of this box</summary>
+		public Vector2 Size {
+			[MethodImpl( INLINE )] get => extents * 2;
+		}
+
+		/// <summary>The width of this rectangle</summary>
+		public float Width {
+			[MethodImpl( INLINE )] get => extents.x * 2;
+		}
+
+		/// <summary>The height of this rectangle</summary>
+		public float Height {
+			[MethodImpl( INLINE )] get => extents.y * 2;
+		}
+	}
+
+	public partial struct Box3D {
+		/// <inheritdoc cref="Box2D.Size"/>
+		public Vector3 Size {
+			[MethodImpl( INLINE )] get => extents * 2;
+		}
+
+		/// <inheritdoc cref="Box2D.Width"/>
+		public float Width {
+			[MethodImpl( INLINE )] get => extents.x * 2;
+		}
+
+		/// <inheritdoc cref="Box2D.Height"/>
+		public float Height {
+			[MethodImpl( INLINE )] get => extents.y * 2;
+		}
+
+		/// <summary>The depth of this box</summary>
+		public float Depth {
+			[MethodImpl( INLINE )] get => extents.z * 2;
+		}
+	}
+
+	#endregion
+
+	#region Min/Max
+
+	public partial struct Box2D {
+		/// <summary>The minimum coordinates inside the box, per axis</summary>
+		public Vector2 Min {
+			[MethodImpl( INLINE )] get => new Vector2( center.x - extents.x, center.y - extents.y );
+		}
+		/// <summary>The maximum coordinates inside the box, per axis</summary>
+		public Vector2 Max {
+			[MethodImpl( INLINE )] get => new Vector2( center.x + extents.x, center.y + extents.y );
+		}
+	}
+
+	public partial struct Box3D {
+		/// <inheritdoc cref="Box2D.Min"/>
+		public Vector3 Min {
+			[MethodImpl( INLINE )] get => new Vector3( center.x - extents.x, center.y - extents.y, center.z - extents.z );
+		}
+		/// <inheritdoc cref="Box2D.Max"/>
+		public Vector3 Max {
+			[MethodImpl( INLINE )] get => new Vector3( center.x + extents.x, center.y + extents.y, center.z + extents.z );
+		}
+	}
+
+	#endregion
+	#region Volume
+
+	public partial struct Box2D {
+		/// <summary>The area of this rectangle</summary>
+		public float Area {
+			[MethodImpl( INLINE )] get => ( extents.x * extents.y ) * 4;
+		}
+	}
+
+	public partial struct Box3D {
+		/// <summary>The volume of this cuboid</summary>
+		public float Volume {
+			[MethodImpl( INLINE )] get => ( extents.x * extents.y * extents.z ) * 8;
+		}
+	}
+
+	#endregion
+
+	#region Surface
+
+	public partial struct Box2D {
+		/// <summary>The total perimeter length of this rectangle</summary>
+		public float Perimeter {
+			[MethodImpl( INLINE )] get => 4 * ( extents.x + extents.y );
+		}
+	}
+
+	public partial struct Box3D {
+		/// <summary>The total surface area of this cuboid</summary>
+		public float SurfaceArea {
+			[MethodImpl( INLINE )] get => 4 * ( extents.y * ( extents.x + extents.z ) + extents.z * extents.x );
+		}
+	}
+
+	#endregion
 }
