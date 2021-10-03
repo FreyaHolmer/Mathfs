58f648583edb56efdc20c9e8dd4ee843fb6fab67
Freya Holmér <acegikmo@gmail.com>
2021-10-03 19:56:00 +0200

    added IParamCurve to Bezier3D

--- Curves/Bezier3D.cs
+++ Curves/Bezier3D.cs
@@ -8,22 +8,12 @@ using UnityEngine;
 namespace Freya {
 
 	/// <summary>A generalized 3D bezier curve, with an arbitrary number of control points. If you intend to only draw cubic bezier curves, consider using BezierCubic3D instead</summary>
-	[Serializable] public class Bezier3D {
+	[Serializable] public class Bezier3D : IParamCurve<Vector3> {
 
 		const MethodImplOptions INLINE = MethodImplOptions.AggressiveInlining;
 
 		/// <inheritdoc cref="Bezier2D.points"/>
 		public readonly Vector3[] points;
-
-		/// <inheritdoc cref="Bezier2D.Count"/>
-		public int Count {
-			[MethodImpl( INLINE )] get => points.Length;
-		}
-
-		/// <inheritdoc cref="Bezier2D.Degree"/>
-		public int Degree {
-			[MethodImpl( INLINE )] get => points.Length - 1;
-		}
 
 		/// <inheritdoc cref="Bezier2D(Vector2[])"/>
 		public Bezier3D( Vector3[] points ) => this.points = points;
@@ -34,15 +24,30 @@ namespace Freya {
 			[MethodImpl( INLINE )] set => points[i] = value;
 		}
 
-		/// <inheritdoc cref="Bezier2D.GetPoint(float)"/>
+		#region Core IParamCurve Implementations
+
+		public Vector3 GetStartPoint() => points[0];
+		public Vector3 GetEndPoint() => points[Count - 1];
+
+		public int Count {
+			[MethodImpl( INLINE )] get => points.Length;
+		}
+
+		/// <summary>The degree of the curve, equal to the number of control points minus 1. 2 points = degree 1 (linear), 3 points = degree 2 (quadratic), 4 points = degree 3 (cubic)</summary>
+		public int Degree {
+			[MethodImpl( INLINE )] get => points.Length - 1;
+		}
+
 		public Vector3 GetPoint( float t ) {
 			return B( Degree, 0 );
 
 			Vector3 B( int k, int i ) {
 				if( k == 0 ) return points[i];
-				return Vector3.LerpUnclamped( B( k - 1, i ), B( k - 1, i + 1 ), t );
+				return Vector3.LerpUnclamped( B( k - 1, i ), B( k - 1, i + 1 ), t ); // todo: optimize
 			}
 		}
+
+		#endregion
 
 		/// <inheritdoc cref="Bezier2D.GetDerivative()"/>
 		public Bezier3D GetDerivative() {
