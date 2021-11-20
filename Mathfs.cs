c49211a73f268b69d0a87f24632e71be1c6beff1
Freya Holmér <acegikmo@gmail.com>
2021-11-20 18:07:07 +0100

    added binomial coefficients & factorials

--- Mathfs.cs
+++ Mathfs.cs
@@ -74,6 +74,67 @@ namespace Freya {
 		/// <summary>Returns the base 10 logarithm of the given value</summary>
 		[MethodImpl( INLINE )] public static float Log10( float value ) => (float)Math.Log10( value );
 
+		/// <summary>Returns the binomial coefficient n over k</summary>
+		static int BinomialCoef( uint n, uint k ) => Factorial( n ) / ( Factorial( k ) * Factorial( n - k ) );
+
+		/// <summary>Returns the Factorial of a given value from 0 to 12</summary>
+		/// <param name="value">A value between 0 and 12 (integers can't store the factorial of 13 or above)</param>
+		[MethodImpl( INLINE )] public static int Factorial( uint value ) {
+			if( value <= 12 )
+				return factorialInt[value];
+			if( value <= 20 )
+				throw new OverflowException( $"The Factorial of {value} is too big for integer representation, please use {nameof(FactorialLong)} instead" );
+			throw new OverflowException( $"The Factorial of {value} is too big for integer representation" );
+		}
+
+		/// <summary>Returns the Factorial of a given value from 0 to 20</summary>
+		/// <param name="value">A value between 0 and 20 (neither long nor ulong can store values large enough for the factorial of 21)</param>
+		[MethodImpl( INLINE )] public static long FactorialLong( uint value ) {
+			if( value <= 20 )
+				return factorialLong[value];
+			throw new OverflowException( $"The Factorial of {value} is too big for integer representation, even unsigned longs, soooo, rip" );
+		}
+
+		static readonly long[] factorialLong = {
+			/*0*/ 1,
+			/*1*/ 1,
+			/*2*/ 2,
+			/*3*/ 6,
+			/*4*/ 24,
+			/*5*/ 120,
+			/*6*/ 720,
+			/*7*/ 5040,
+			/*8*/ 40320,
+			/*9*/ 362880,
+			/*10*/ 3628800,
+			/*11*/ 39916800,
+			/*12*/ 479001600,
+			/*13*/ 6227020800,
+			/*14*/ 87178291200,
+			/*15*/ 1307674368000,
+			/*16*/ 20922789888000,
+			/*17*/ 355687428096000,
+			/*18*/ 6402373705728000,
+			/*19*/ 121645100408832000,
+			/*20*/ 2432902008176640000
+		};
+
+		static readonly int[] factorialInt = {
+			/*0*/ 1,
+			/*1*/ 1,
+			/*2*/ 2,
+			/*3*/ 6,
+			/*4*/ 24,
+			/*5*/ 120,
+			/*6*/ 720,
+			/*7*/ 5040,
+			/*8*/ 40320,
+			/*9*/ 362880,
+			/*10*/ 3628800,
+			/*11*/ 39916800,
+			/*12*/ 479001600
+		};
+
 		#endregion
 
 		#region Floating point shenanigans
