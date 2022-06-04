37df088e5f7c3c1299f74746faa870b9dd57c292
Freya Holmér <acegikmo@gmail.com>
2022-06-05 00:03:05 +0200

    started work on codegen for simple spline segments

---
+++ Codegen/Editor/CodeGenerator.cs
@@ -0,0 +1,66 @@
+// by Freya Holmér (https://github.com/FreyaHolmer/Mathfs)
+
+using System;
+using System.Collections.Generic;
+
+namespace Freya {
+
+	public class CodeGenerator {
+
+		int scope = 0;
+		public List<string> content = new List<string>();
+
+		public void Append( string s ) {
+			content.Add( $"{new string( '\t', scope )}{s}" );
+		}
+
+		public void Comment( string s ) => Append( $"// {s}" );
+		public void Using( string s ) => Append( $"using {s};" );
+		public void Summary( string s ) => Append( $"/// <summary>{s}</summary>" );
+		public void Param( string param, string desc ) => Append( $"/// <param name=\"{param}\">{desc}</param>" );
+
+		public void LineBreak() => content.Add( "" );
+
+		public CodeScope BracketScope( string s ) => new CodeScope( this, s, true );
+		public CodeScope Scope( string s ) => new CodeScope( this, s, false );
+		public RegionScope ScopeRegion( string s ) => new RegionScope( this, s );
+
+		public readonly struct CodeScope : IDisposable {
+
+			readonly CodeGenerator gen;
+			readonly bool includeBrackets;
+
+			public CodeScope( CodeGenerator gen, string s, bool includeBrackets = true ) {
+				this.gen = gen;
+				this.includeBrackets = includeBrackets;
+				gen.Append( includeBrackets ? $"{s} {{" : s );
+				gen.scope++;
+			}
+
+			public void Dispose() {
+				gen.scope--;
+				if( includeBrackets )
+					gen.Append( "}" );
+			}
+		}
+
+		public readonly struct RegionScope : IDisposable {
+
+			readonly CodeGenerator gen;
+
+			public RegionScope( CodeGenerator gen, string s ) {
+				this.gen = gen;
+				gen.LineBreak();
+				gen.Append( $"#region {s}" );
+				gen.LineBreak();
+			}
+
+			public void Dispose() {
+				gen.LineBreak();
+				gen.Append( "#endregion" );
+				gen.LineBreak();
+			}
+		}
+	}
+
+}
---
+++ Codegen/Editor/MathfsCodegen.cs
@@ -0,0 +1,287 @@
+// by Freya Holmér (https://github.com/FreyaHolmer/Mathfs)
+
+using System;
+using System.IO;
+using System.Linq;
+using UnityEditor;
+
+namespace Freya {
+
+	public static class MathfsCodegen {
+
+		class SplineType {
+			public int degree;
+			public string className;
+			public string prettyName;
+			public string prettyNameLower;
+			public string[] paramNames;
+			public string[] paramDescs;
+			public string matrixName;
+
+			public SplineType( int degree, string className, string prettyName, string matrixName, string[] paramNames, string[] paramDescs, string[] paramDescsQuad = null ) {
+				this.degree = degree;
+				this.className = className;
+				this.prettyName = prettyName;
+				this.prettyNameLower = prettyName.ToLowerInvariant();
+				this.paramDescs = paramDescs;
+				this.matrixName = matrixName;
+				this.paramNames = paramNames;
+			}
+
+			public void AppendParamStrings( CodeGenerator gen, int degree, int i ) {
+				gen.Param( paramNames[i], paramDescs[i] );
+			}
+		}
+
+		#region Type Definitions
+
+		static SplineType typeBezier = new SplineType( 3, "Bezier", "Bézier", "cubicBezier",
+			new[] { "p0", "p1", "p2", "p3" },
+			new[] {
+				"The starting point of the curve",
+				"The second control point of the curve, sometimes called the start tangent point",
+				"The third control point of the curve, sometimes called the end tangent point",
+				"The end point of the curve"
+			}
+		);
+
+		static SplineType typeBezierQuad = new SplineType( 2, "Bezier", "Bézier", "quadraticBezier",
+			new[] { "p0", "p1", "p2" },
+			new[] {
+				"The starting point of the curve",
+				"The middle control point of the curve, sometimes called a tangent point",
+				"The end point of the curve"
+			}
+		);
+
+		static SplineType typeHermite = new SplineType( 3, "Hermite", "Hermite", "cubicHermite",
+			new[] { "p0", "v0", "p1", "v1" },
+			new[] {
+				"The starting point of the curve",
+				"The rate of change (velocity) at the start of the curve",
+				"The end point of the curve",
+				"The rate of change (velocity) at the end of the curve"
+			}
+		);
+
+		static SplineType typeBspline = new SplineType( 3, "UBS", "B-Spline", "cubicUniformBspline",
+			new[] { "p0", "p1", "p2", "p3" },
+			new[] {
+				"The first point of the B-spline hull",
+				"The second point of the B-spline hull",
+				"The third point of the B-spline hull",
+				"The fourth point of the B-spline hull"
+			}
+		);
+
+		static SplineType typeCatRom = new SplineType( 3, "CatRom", "Catmull-Rom", "cubicCatmullRom",
+			new[] { "p0", "p1", "p2", "p3" },
+			new[] {
+				"The first control point of the catmull-rom curve. Note that this point is not included in the curve itself, and only helps to shape it",
+				"The second control point, and the start of the catmull-rom curve",
+				"The third control point, and the end of the catmull-rom curve",
+				"The last control point of the catmull-rom curve. Note that this point is not included in the curve itself, and only helps to shape it"
+			}
+		);
+
+		#endregion
+
+		[MenuItem( "Assets/Run Mathfs Codegen" )]
+		public static void Regenerate() {
+			for( int dim = 1; dim < 4; dim++ ) { // 1D, 2D, 3D
+				GenerateType( typeBezier, dim );
+				GenerateType( typeBezierQuad, dim );
+				GenerateType( typeHermite, dim );
+				GenerateType( typeBspline, dim );
+				GenerateType( typeCatRom, dim );
+			}
+		}
+
+		public static string GetLerpName( int dim ) {
+			return dim switch {
+				1 => "Mathfs.Lerp",
+				2 => "Vector2.LerpUnclamped",
+				3 => "Vector3.LerpUnclamped",
+				4 => "Vector4.LerpUnclamped",
+				_ => throw new IndexOutOfRangeException()
+			};
+		}
+
+		static void GenerateType( SplineType type, int dim ) {
+			int degree = type.degree;
+			string dataType = dim == 1 ? "float" : $"Vector{dim}";
+			string polynomType = dim == 1 ? "Polynomial" : $"Polynomial{dim}D";
+			int ptCount = degree + 1;
+			string degFullLower = GetDegreeName( degree, false );
+			string degShortCapital = GetDegreeName( degree, true );
+			string structName = $"{type.className}{degShortCapital}{dim}D";
+			string[] points = type.paramNames;
+			string[] pointDescs = type.paramDescs;
+			string lerpName = GetLerpName( dim );
+			string curveFunc = dim == 1 ? "GetEvalPolynomial" : "GetCurve";
+
+			CodeGenerator code = new CodeGenerator();
+			code.Comment( "by Freya Holmér (https://github.com/FreyaHolmer/Mathfs)" );
+			code.LineBreak();
+			code.Using( "System" );
+			code.Using( "System.Runtime.CompilerServices" );
+			code.Using( "UnityEngine" );
+			code.LineBreak();
+
+			using( code.BracketScope( "namespace Freya" ) ) {
+				code.LineBreak();
+
+				// type definition
+				code.Summary( $"An optimized uniform {dim}D {degFullLower} {type.prettyNameLower} segment, with {ptCount} control points" );
+				using( code.BracketScope( $"[Serializable] public struct {structName} : IParamCubicSplineSegment{dim}D" ) ) { // intentionally always Cubic right now
+					code.LineBreak();
+					code.Append( "const MethodImplOptions INLINE = MethodImplOptions.AggressiveInlining;" );
+					code.LineBreak();
+
+					// constructor
+					code.Summary( $"Creates a uniform {dim}D {degFullLower} {type.prettyNameLower} segment, from {ptCount} control points" );
+					for( int i = 0; i < ptCount; i++ )
+						type.AppendParamStrings( code, degree, i );
+					using( code.BracketScope( $"public {structName}( {string.Join( ", ", points.Select( p => $"{dataType} {p}" ) )} )" ) ) {
+						code.Append( $"( {string.Join( ", ", points.Select( p => $"this.{p}" ) )} ) = ( {string.Join( ", ", points )} );" );
+						code.Append( "validCoefficients = false;" );
+						code.Append( "curve = default;" );
+					}
+
+					code.LineBreak();
+
+					// Curve
+					code.Append( $"{polynomType} curve;" );
+					using( code.BracketScope( $"public {polynomType} Curve" ) ) {
+						using( code.BracketScope( $"get" ) ) {
+							code.Append( "ReadyCoefficients();" );
+							code.Append( "return curve;" );
+						}
+					}
+
+					// control point properties
+					using( code.ScopeRegion( "Control Points" ) ) {
+						code.Append( $"[SerializeField] {dataType} {string.Join( ", ", points )};" );
+						code.LineBreak();
+						for( int i = 0; i < ptCount; i++ ) {
+							code.Summary( pointDescs[i] );
+							using( code.BracketScope( $"public {dataType} {points[i].ToUpperInvariant()}" ) ) {
+								code.Append( $"[MethodImpl( INLINE )] get => {points[i]};" );
+								code.Append( $"[MethodImpl( INLINE )] set => _ = ( {points[i]} = value, validCoefficients = false );" );
+							}
+
+							code.LineBreak();
+						}
+
+						code.Summary( $"Get or set a control point position by index. Valid indices from 0 to {degree}" );
+						using( code.BracketScope( $"public {dataType} this[ int i ]" ) ) {
+							using( code.Scope( "get =>" ) ) {
+								using( code.Scope( "i switch {" ) ) {
+									for( int i = 0; i < ptCount; i++ )
+										code.Append( $"{i} => {points[i].ToUpperInvariant()}," );
+									code.Append( $"_ => throw new ArgumentOutOfRangeException( nameof(i), $\"Index has to be in the 0 to {degree} range, and I think {{i}} is outside that range you know\" )" );
+								}
+
+								code.Append( "};" );
+							}
+
+							using( code.BracketScope( "set" ) ) {
+								using( code.BracketScope( "switch( i )" ) ) {
+									for( int i = 0; i < ptCount; i++ ) {
+										using( code.Scope( $"case {i}:" ) ) {
+											code.Append( $"{points[i].ToUpperInvariant()} = value;" );
+											code.Append( "break;" );
+										}
+									}
+
+									code.Append( $"default: throw new ArgumentOutOfRangeException( nameof(i), $\"Index has to be in the 0 to {degree} range, and I think {{i}} is outside that range you know\" );" );
+								}
+							}
+						}
+					}
+
+					// Coefficients
+					using( code.ScopeRegion( "Coefficients" ) ) {
+						code.Append( "[NonSerialized] bool validCoefficients;" );
+						code.LineBreak();
+						using( code.BracketScope( "[MethodImpl( INLINE )] void ReadyCoefficients()" ) ) {
+							using( code.Scope( "if( validCoefficients )" ) )
+								code.Append( "return; // no need to update" );
+							code.Append( "validCoefficients = true;" );
+							code.Append( $"curve = CharMatrix.{type.matrixName}.{curveFunc}( {string.Join( ", ", points )} );" );
+						}
+					}
+
+					// equality checks
+					using( code.ScopeRegion( "Object Comparison & ToString" ) ) {
+						code.Append( $"public static bool operator ==( {structName} a, {structName} b ) => {string.Join( " && ", points.Select( p => $"a.{p.ToUpperInvariant()} == b.{p.ToUpperInvariant()}" ) )};" );
+						code.Append( $"public static bool operator !=( {structName} a, {structName} b ) => !( a == b );" );
+						code.Append( $"public bool Equals( {structName} other ) => {string.Join( " && ", points.Select( p => $"{p.ToUpperInvariant()}.Equals( other.{p.ToUpperInvariant()} )" ) )};" );
+						code.Append( $"public override bool Equals( object obj ) => obj is {structName} other && Equals( other );" );
+						code.Append( $"public override int GetHashCode() => HashCode.Combine( {string.Join( ", ", points )} );" );
+						code.LineBreak();
+						code.Append( $"public override string ToString() => $\"({string.Join( ", ", points.Select( p => $"{{{p}}}" ) )})\";" );
+					}
+
+					// Interpolation
+					using( code.ScopeRegion( "Interpolation" ) ) {
+						code.Summary( $"Returns a linear blend between two {type.prettyNameLower} curves" );
+						code.Param( "a", "The first spline segment" );
+						code.Param( "b", "The second spline segment" );
+						code.Param( "t", "A value from 0 to 1 to blend between <c>a</c> and <c>b</c>" );
+						using( code.Scope( $"public static {structName} Lerp( {structName} a, {structName} b, float t ) =>" ) ) {
+							using( code.Scope( "new(" ) ) {
+								for( int i = 0; i < ptCount; i++ ) {
+									code.Append( $"{lerpName}( a.{points[i]}, b.{points[i]}, t )" + ( i == ptCount - 1 ? "" : "," ) );
+								}
+							}
+
+							code.Append( ");" );
+						}
+					}
+
+					// special case slerps for cubic beziers in 2D and 3D
+					if( dim > 1 && degree is 2 or 3 && type == typeBezier ) {
+						string slerpCast = dim == 2 ? "(Vector2)" : "";
+						code.LineBreak();
+						code.Summary( $"Returns a linear blend between two {type.prettyNameLower} curves, where the tangent directions are spherically interpolated" );
+						code.Param( "a", "The first spline segment" );
+						code.Param( "b", "The second spline segment" );
+						code.Param( "t", "A value from 0 to 1 to blend between <c>a</c> and <c>b</c>" );
+						using( code.BracketScope( $"public static {structName} Slerp( {structName} a, {structName} b, float t )" ) ) {
+							code.Append( $"{dataType} p0 = {lerpName}( a.p0, b.p0, t );" );
+							code.Append( $"{dataType} p3 = {lerpName}( a.p3, b.p3, t );" );
+							using( code.Scope( $"return new {structName}(" ) ) {
+								code.Append( $"p0," );
+								code.Append( $"p0 + {slerpCast}Vector3.SlerpUnclamped( a.p1 - a.p0, b.p1 - b.p0, t )," );
+								code.Append( $"p3 + {slerpCast}Vector3.SlerpUnclamped( a.p2 - a.p3, b.p2 - b.p3, t )," );
+								code.Append( $"p3" );
+							}
+
+							code.Append( ");" );
+						}
+					}
+
+
+					// todo: conversion to other spline types
+				}
+			}
+
+			string path = $"Assets/Mathfs/Splines/Uniform Spline Segments/{structName}.cs";
+			File.WriteAllLines( path, code.content );
+		}
+
+		public static string GetDegreeName( int d, bool shortName ) {
+			return d switch {
+				1 => "Linear",
+				2 => shortName ? "Quad" : "Quadratic",
+				3 => "Cubic",
+				4 => "Quartic",
+				5 => "Quintic",
+				_ => throw new IndexOutOfRangeException()
+			};
+		}
+
+	}
+
+}
