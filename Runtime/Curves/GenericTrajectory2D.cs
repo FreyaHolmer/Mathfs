285d96da4209b05dd7df2f481b897dc8a42622bc
Freya Holmér <acegikmo@gmail.com>
2022-10-18 16:13:50 +0200

    n-degree generic 2D trajectory

---
+++ Runtime/Curves/GenericTrajectory2D.cs
@@ -0,0 +1,23 @@
+// by Freya Holmér (https://github.com/FreyaHolmer/Mathfs)
+
+using Freya;
+using UnityEngine;
+
+public class GenericTrajectory2D {
+
+	public Vector2[] derivatives;
+
+	public GenericTrajectory2D( params Vector2[] derivatives ) => this.derivatives = derivatives;
+
+	public Vector2 GetPosition( float time ) {
+		Vector2 pt = derivatives[0];
+		for( int i = 1; i < derivatives.Length; i++ ) {
+			float scale = Mathfs.Pow( time, i ) / Mathfs.Factorial( (uint)i );
+			pt += scale * derivatives[i];
+		}
+
+		return pt;
+	}
+
+
+}
