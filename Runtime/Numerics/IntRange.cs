b79257ad8d2d459a9945e868caa72ba31c5c4874
Freya Holmér <acegikmo@gmail.com>
2022-11-17 21:51:53 +0100

    Add IntRange.cs

---
+++ Runtime/Numerics/IntRange.cs
@@ -0,0 +1,10 @@
+public readonly struct IntRange {
+	public readonly int start;
+	public readonly int count;
+
+	public IntRange( int start, int count ) {
+		this.start = start;
+		this.count = count;
+	}
+
+}
