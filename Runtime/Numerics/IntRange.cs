cdeae8b4f89eb4c0307c069f15e1f5d381369fd2
Freya Holmér <acegikmo@gmail.com>
2022-12-25 18:02:40 +0100

    int range tweaks

--- Runtime/Numerics/IntRange.cs
+++ Runtime/Numerics/IntRange.cs
@@ -10,14 +10,14 @@ namespace Freya {
 		public readonly int count;
 
 		/// <summary>The last integer in the range</summary>
-		public int End => start + count - 1;
+		public int Last => start + count - 1;
 
 		/// <summary>The distance from first to last integer. Equivalent to <c>count-1</c></summary>
 		public int Distance => count - 1;
 
-		/// <summary>Creates a new integer range, given a start integer and how many more integers to include</summary>
+		/// <summary>Creates a new integer range, given a start integer and how many integers to include in total</summary>
 		/// <param name="start">The first integer</param>
-		/// <param name="count">How many integers to include in the range</param>
+		/// <param name="count">How many integers to include in the full range</param>
 		public IntRange( int start, int count ) {
 			this.start = start;
 			this.count = count;
@@ -25,18 +25,19 @@ namespace Freya {
 
 		/// <summary>Whether or not this range contains a given value (inclusive)</summary>
 		/// <param name="value">The value to check if it's inside, or equal to the start or end</param>
-		public bool Contains( int value ) => value >= start && value <= End;
+		public bool Contains( int value ) => value >= start && value <= Last;
 
 		/// <summary>Create an integer range from start to end (inclusive)</summary>
-		/// <param name="start">The first integer</param>
-		/// <param name="end">The last integer (inclusive)</param>
-		public static IntRange StartEnd( int start, int end ) => new IntRange( start, end - start + 1 );
-		
+		/// <param name="first">The first integer</param>
+		/// <param name="last">The last integer</param>
+		public static IntRange FirstToLast( int first, int last ) => new IntRange( first, last - first + 1 );
+
 		static readonly StringBuilder toStrBuilder = new StringBuilder();
+
 		public override string ToString() {
 			toStrBuilder.Clear();
 			toStrBuilder.Append( "{ " );
-			int last = End;
+			int last = Last;
 			for( int i = start; i <= last; i++ ) {
 				toStrBuilder.Append( i );
 				if( i != last )
