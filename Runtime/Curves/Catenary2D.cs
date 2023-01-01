3f5ad9cd577a75c98a0e728e00808a1aa3970e74
Freya Holmér <acegikmo@gmail.com>
2023-01-01 14:21:47 +0100

    lots of catenary2D cleanup, now arc len param.

--- Runtime/Curves/Catenary2D.cs
+++ Runtime/Curves/Catenary2D.cs
@@ -37,16 +37,34 @@ namespace Freya {
 		}
 
 		const int INTERVAL_SEARCH_ITERATIONS = 12;
-		const int BISECT_REFINE_COUNT = 15;
+		const int BISECT_REFINE_COUNT = 14;
 
-		// input data
-		readonly Vector2 p0, p1;
-		readonly float s;
-
+		// data
+		Vector2 p0, p1;
+		float s;
+		
 		// cached state
 		float a;
+		Vector2 p;
 		Vector2 delta;
+		float arcLenSampleOffset;
 		Evaluability evaluability;
+		
+		public float Length {
+			get => s;
+			set => ( s, evaluability ) = ( value, Evaluability.Unknown );
+		}
+		public Vector2 P0 {
+			get => p0;
+			set => ( p0, evaluability ) = ( value, Evaluability.Unknown );
+		}
+		public Vector2 P1 {
+			get => p1;
+			set => ( p1, evaluability ) = ( value, Evaluability.Unknown );
+		}
+
+		public bool IsVertical => Mathf.Abs( p.x ) < 0.001f;
+		public bool IsStraightLine => s <= Vector2.Distance( p0, p1 ) * 1.00005f;
 
 		/// <summary>Creates a catenary curve between two points, given an arc length <c>s</c></summary>
 		/// <param name="p0">The start of the curve</param>
@@ -55,55 +73,73 @@ namespace Freya {
 		public Catenary2D( Vector2 p0, Vector2 p1, float s ) {
 			( this.p0, this.p1, this.s ) = ( p0, p1, s );
 			a = 0;
+			p = default;
 			delta = default;
+			arcLenSampleOffset = default;
 			evaluability = Evaluability.Unknown;
 		}
 
 		/// <summary>Evaluates a position on this catenary curve, given a <c>t</c>-value from 0 to 1</summary>
 		/// <param name="t">A value from 0 to 1 along the whole curve</param>
-		public Vector2 Eval( float t ) {
+		public Vector2 Eval( float t ) => EvalByArcLength( t * s );
+
+		/// <summary>Evaluates a position on this catenary curve at the given arc length of <c>sEval</c></summary>
+		/// <param name="sEval">The arc length along the curve to sample, relative to the first point</param>
+		public Vector2 EvalByArcLength( float sEval ) {
 			ReadyForEvaluation();
+			return evaluability switch {
+				Evaluability.Catenary       => EvalCatPosByArcLength( sEval ),
+				Evaluability.LineSegment    => EvalStraightLineByArcLength( sEval ),
+				Evaluability.LinearVertical => EvalVerticalLinearApproxByArcLength( sEval ),
+				Evaluability.Unknown or _   => throw new Exception( "Failed to evaluate catenary, couldn't calculate evaluability" )
+			};
+		}
 
-			if( evaluability == Evaluability.LineSegment )
-				return Vector3.LerpUnclamped( p0, p1, t ); // chain is almost completely linear
+		// straight line from p0 to p1
+		Vector2 EvalStraightLineByArcLength( float sEval ) => Vector3.LerpUnclamped( p0, p1, sEval / s );
 
-			Vector2 p = p1 - p0;
-			float x = Mathfs.Lerp( 0, p.x, t );
-			float y;
-			if( evaluability == Evaluability.LinearVertical ) { // chain is almost completely vertical, use a linear approximation
-				float ts = t * s;
-				float seg0 = ( s - p.y ) / 2;
-				y = ( ts < seg0 ) ? -ts : -2 * seg0 + ts;
-			} else {
-				y = EvalFrom0( x );
-			}
-
+		// almost completely vertical line when p0.x is approx. equal to p1.x
+		Vector2 EvalVerticalLinearApproxByArcLength( float sEval ) {
+			float x = Mathfs.Lerp( 0, p.x, sEval / s ); // just to make it not snap to x=0
+			float b = ( p.y - s ) / 2; // bottom
+			float seg0 = -b;
+			float y = ( sEval < seg0 ) ? -sEval : -2 * seg0 + sEval;
 			return new Vector2( x, y ) + p0;
 		}
 
-		// Passing through (0,0) and point p
-		float EvalFrom0( float x ) => a * Mathfs.Cosh( ( x - delta.x ) / a ) + delta.y;
+		// evaluates the position of the catenary at the given arc length, relative to the first point
+		Vector2 EvalCatPosByArcLength( float sEval ) {
+			float x = EvalCatXByArcLengthPassingThrough0( sEval );
+			float y = EvalPassingThrough0( x );
+			return new Vector2( x, y ) + p0;
+		}
 
-		bool IsFullyVertical( float dx ) => Mathf.Abs( dx ) < 0.001f;
-		bool IsStraightLine() => s <= Vector2.Distance( p0, p1 ) * 1.00005f;
+		float EvalCatXByArcLengthPassingThrough0( float sEval ) {
+			sEval *= p.x.Sign(); // since we go backwards when p0.x < p1.x
+			return Catenary2D.EvalXByArcLength( sEval + arcLenSampleOffset, a ) + delta.x;
+		}
 
+		// Evaluate passing through the origin and p
+		float EvalPassingThrough0( float x ) => Catenary2D.Eval( x - delta.x, a ) + delta.y;
+
+		// calculates p, a, delta, arcLenSampleOffset, and which evaluation method to use
 		void ReadyForEvaluation() {
 			if( evaluability != Evaluability.Unknown )
 				return;
 
+			// cache p, ie: p1 relative to p0
+			p = p1 - p0;
+
 			// CASE 1:
 			// first, test if it's a line segment
-			if( IsStraightLine() ) {
+			if( IsStraightLine ) {
 				evaluability = Evaluability.LineSegment;
 				return;
 			}
 
-			// relative to origin point p
-			Vector2 p = p1 - p0;
-
 			// CASE 2:
 			// check if it's basically a fully vertical hanging chain
-			if( IsFullyVertical( p.x ) ) {
+			if( IsVertical ) {
 				evaluability = Evaluability.LinearVertical;
 				return;
 			}
@@ -122,6 +158,7 @@ namespace Freya {
 					RootFindBisections( R, ref xRange, BISECT_REFINE_COUNT ); // Catenary seems valid, with roots inside, refine the range
 				a = xRange.Center; // set a to the middle of the latest range
 				delta = CalcCatenaryDelta( a, p ); // find delta to pass through both points
+				arcLenSampleOffset = CalcArcLenSampleOffset( delta.x, a );
 				evaluability = Evaluability.Catenary;
 			} else {
 				// CASE 4:
@@ -130,11 +167,14 @@ namespace Freya {
 			}
 		}
 
+		// Calculates the arc length offset so that it's relative to the start of the chain when evaluating by arc length
+		static float CalcArcLenSampleOffset( float deltaX, float a ) => Catenary2D.EvalArcLen( -deltaX, a );
+
 		// Calculates the required offset to make a catenary pass through the origin and a point p
 		static Vector2 CalcCatenaryDelta( float a, Vector2 p ) {
 			Vector2 d;
 			d.x = p.x / 2 - a * Mathfs.Asinh( p.y / ( 2 * a * Mathfs.Sinh( p.x / ( 2 * a ) ) ) );
-			d.y = -a * Mathfs.Cosh( -d.x / a );
+			d.y = -Catenary2D.Eval( d.x, a ); // technically -d.x but because of symmetry d.x works too
 			return d;
 		}
 
