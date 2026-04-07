4fb9f804f944170b70f0a5c5ce80002d8d0c6653
Freya Holmér <acegikmo@gmail.com>
2026-04-07 20:29:43 +0200

    Interfaces for roundable types

---
+++ Runtime/Numerics/IRoundable.cs
@@ -0,0 +1,23 @@
+﻿// by Freya Holmér (https://github.com/FreyaHolmer/Mathfs)
+
+namespace Freya {
+
+	/// <summary>Basically the same as C#'s <see cref="System.MidpointRounding"/>, but older .net versions don't have all the options</summary>
+	public enum RoundingDirection {
+		ToEven = 0,
+		AwayFromZero = 1,
+		ToZero = 2,
+		ToNegativeInfinity = 3,
+		ToPositiveInfinity = 4,
+	}
+
+	/// <summary>Objects that can be rounded to nearby values</summary>
+	public interface IRoundable<out R> {
+		public R round( RoundingDirection rounding = RoundingDirection.ToEven );
+		public R floorToward0 { get; }
+		public R ceilAwayFrom0 { get; }
+		public R floor { get; }
+		public R ceil { get; }
+	}
+
+}
