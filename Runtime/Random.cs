47cd76a7f7366611fd3a55087e24931d3826a8cf
Freya Holmér <acegikmo@gmail.com>
2026-04-07 22:32:24 +0200

    Gaussian sampling

--- Runtime/Random.cs
+++ Runtime/Random.cs
@@ -1,5 +1,6 @@
 ﻿// by Freya Holmér (https://github.com/FreyaHolmer/Mathfs)
 
+using System;
 using UnityEngine;
 using static Freya.Mathfs;
 using UnityRandom = UnityEngine.Random;
@@ -62,6 +63,37 @@ namespace Freya {
 		// 3D Orientation
 		/// <summary>Returns a random uniformly distributed rotation</summary>
 		public static Quaternion Rotation => UnityRandom.rotationUniform;
+
+		// Normal distributions
+		/// <summary>Returns random numbers with a gaussian distribution</summary>
+		/// <param name="variance">The distance away from the mean that will contain 68.27% of all values</param>
+		/// <param name="mean">The mean (average) value of the distribution</param>
+		public static float Gaussian( float variance = 1, float mean = 0 ) {
+			float u1 = UnityEngine.Random.value;
+			float u2 = UnityEngine.Random.value;
+			return MathF.Sqrt( -2 * MathF.Log( u1 ) ) * MathF.Cos( TAU * u2 ) * variance + mean;
+		}
+
+		/// <summary>Samples a gaussian with rejection sampling, retrying when samples end up outside the given range</summary>
+		/// <param name="variance">The distance away from the mean that will contain 68.27% of all values</param>
+		/// <param name="mean">The mean (average) value of the distribution</param>
+		/// <param name="min">The minimum value of the range</param>
+		/// <param name="max">The maximum value of the range</param>
+		/// <param name="samplingMaxAttempts">The maximum number of times to attempt to get a value within the range, before giving up and clamping instead</param>
+		public static float GaussianInRange( float variance = 1, float mean = 0, float min = -1, float max = 1, int samplingMaxAttempts = 3 ) {
+			float x;
+			int attempts = 0;
+			do {
+				x = Gaussian( variance, mean );
+				attempts++;
+			} while( attempts < samplingMaxAttempts && x.Within( min, max ) == false );
+			return x.Clamp( min, max );
+		}
+
+		/// <summary>Returns a value between -1 and 1 with a variance of 1/4</summary>
+		public static float GaussianNeg1to1 => GaussianInRange( 1 / 4f );
+		/// <summary>Returns a value between 0 and 1 with a variance of 1/8</summary>
+		public static float Gaussian01 => GaussianInRange( 1 / 8f, 1 / 2f, 0, 1 );
 	}
 
 }
