8d1c471846681f7195d305d93a46cd9c8d4fbe79
Freya Holmér <acegikmo@gmail.com>
2026-04-08 04:27:57 +0200

    Added Pairs enumeration extension

---
+++ Runtime/Numerics/EnumerationExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace Freya {
+
+	public static class EnumerationExtensions {
+
+		public static IEnumerable<(T a, T b)> Pairs<T>( this IEnumerable<T> items, bool loop ) {
+			bool hasFoundFirst = false;
+			T first = default;
+			T prev = default;
+			foreach( T item in items ) {
+				if( hasFoundFirst == false ) {
+					hasFoundFirst = true;
+					first = item;
+				} else {
+					yield return ( prev, item );
+				}
+				prev = item;
+			}
+			if( loop && hasFoundFirst )
+				yield return ( prev, first );
+		}
+
+	}
+
+}
